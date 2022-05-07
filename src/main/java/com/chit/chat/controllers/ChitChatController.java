47e3796a6a8808b6b6339a9165c81195f1978806
Jishnuvas <45481518+Jishnuvas@users.noreply.github.com>
2022-05-08 00:28:42 +0530

    Update ChitChatController.java

--- src/main/java/com/chit/chat/controllers/ChitChatController.java
+++ src/main/java/com/chit/chat/controllers/ChitChatController.java
@@ -1,6 +1,9 @@
 package com.chit.chat.controllers;
 
+import com.chit.chat.listenerEvents.SocketEventListener;
 import com.chit.chat.models.ChitChatPojoModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -10,18 +13,29 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ChitChatController {
 
-    @MessageMapping("chat.sendMessage")
+    private static final Logger logger = LoggerFactory.getLogger ( SocketEventListener.class );
+    @MessageMapping("sendMessage")
     @SendTo("/topic/group")
-    public ChitChatPojoModel sendMessage( @Payload ChitChatPojoModel message,SimpMessageHeaderAccessor headerAccessor ){
-        return message;
+    public ChitChatPojoModel sendMessage( @Payload ChitChatPojoModel message,SimpMessageHeaderAccessor headerAccessor  ){
+        String username = (String) headerAccessor.getSessionAttributes ().get("username");
+        if(username != null) {
+            logger.info ( "User" + username );
+            ChitChatPojoModel newMessage = new ChitChatPojoModel ();
+            newMessage.setUser ( username );
+            newMessage.setType ( ChitChatPojoModel.MessageType.CHAT );
+            newMessage.setContent (message.getContent ()  );
+            return newMessage;
+        }else{
+            return null;
+        }
     }
 
 
-    @MessageMapping("chat.addUser")
+    @MessageMapping("addUser")
     @SendTo("/topic/group")
     public ChitChatPojoModel addUser( @Payload ChitChatPojoModel message, SimpMessageHeaderAccessor headerAccessor ){
         // adding username in socket session
-        headerAccessor.getSessionAttributes ().put ( "username" ,message.getSender ());
+        headerAccessor.getSessionAttributes ().put ( "username" ,message.getUser ());
         return message;
     }
 
